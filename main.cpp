93627b5f99d54b9598fe9f5e7249d07992d6b1e4
Alex Bogushenko <bogushenkoas@ukr.net>
2023-10-10 21:57:22 +0300

    finish commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,84 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
+vector <string> all_order;
+vector <string> ready_dish;
+mutex mtx;
+mutex mtx2;
+string dish;
+
+// прием заказов
+void orders()
+{
+  string food[5] = {"pizza", "soup", "steak", "salad", "sushi"};
+  string name_food;   
+  while(1)
+  {
+    int time1 = rand() % 6 + 5;
+    this_thread :: sleep_for(chrono :: seconds(time1));
+    int num = rand() % sizeof(food) / sizeof(food[0]);
+    name_food = food[num];
+    cout << "the waiter took a new order for: " << name_food  << endl;
+    mtx.lock();
+    all_order.push_back(name_food);
+    mtx.unlock();
+
+  }
+}
+
+// приготовление заказа
+void chickens()
+{
+  while(1)
+  {
+    mtx2.lock();
+    int time2 = rand() % 11 + 5;
+    this_thread :: sleep_for(chrono :: seconds(time2));
+    dish = all_order[0];
+    all_order.erase(all_order.begin()); 
+    ready_dish.push_back(dish);
+    cout << "dish " << dish << " ready" << endl;
+    mtx2.unlock();
+  }
+    
+}
+
+
+
+    
 
 int main()
 {
-    cout << "Hello Alex !!!";
+    srand(time(0));
+    int count = 1;
+    
+    thread order(orders);
+    order.detach();
+
+    thread chicken(chickens);
+    chicken.detach();
+
+    // доставка заказа 
+    while(count <= 10)
+    {
+      this_thread :: sleep_for(chrono :: seconds(30));
+      cout << "the courier picked up the order " << count ++ << endl;
+      for(int i = 0; i < ready_dish.size(); i ++)
+      {
+          cout << ready_dish[i] << endl;
+      }
+
+      ready_dish.clear();
+
+    }
+  
+
+ return 0;
+  
+    
 }
